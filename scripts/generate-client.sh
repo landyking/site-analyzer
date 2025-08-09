a98317c842718a0d25eef572319384ed675a4776
landyking <landykingdom@gmail.com>
2025-08-09 18:08:17 +1200

    web: generate client code

---
+++ scripts/generate-client.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+cd backend
+python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
+cd ..
+mv openapi.json webfront/
+cd webfront
+npm run generate-client
+npx biome format --write ./src/client
