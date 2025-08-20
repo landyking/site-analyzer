97deb925bbef1bc89202b72dbe4fd6cec8bf2e00
landyking <landykingdom@gmail.com>
2025-08-21 11:27:34 +1200

    web: polish

--- webfront/src/crud-dashboard/map-details/MapTab.tsx
+++ webfront/src/crud-dashboard/map-details/MapTab.tsx
@@ -1,7 +1,7 @@
 
 
 import Box from '@mui/material/Box';
-import Alert from '@mui/material/Alert';
+// import Alert from '@mui/material/Alert';
 import type { MapTaskDetails } from '../../client/types.gen';
 
 interface MapTabProps {
@@ -11,9 +11,9 @@ interface MapTabProps {
 
 const MapTab: React.FC<MapTabProps> = ({ mapTask }) => (
   <Box sx={{ p: 2 }}>
-    <Alert severity="info" sx={{ background: '#eaf6fb', color: '#1976d2', mb: 2 }}>
+    {/* <Alert severity="info" sx={{ background: '#eaf6fb', color: '#1976d2', mb: 2 }}>
       The map task status is pending / processing.
-    </Alert>
+    </Alert> */}
     <pre style={{ fontSize: 12, background: '#f5f5f5', padding: 12, borderRadius: 4, overflowX: 'auto' }}>
       {JSON.stringify(mapTask, null, 2)}
     </pre>
