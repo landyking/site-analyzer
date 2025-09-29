9c19e20cc369274acdc01250ed6d73e4bc80a847
landyking <landykingdom@gmail.com>
2025-09-29 22:21:50 +1300

    web: refactor FinalSection

--- webfront/src/crud-dashboard/components/map-details/report-components/sections/FinalSection.tsx
+++ webfront/src/crud-dashboard/components/map-details/report-components/sections/FinalSection.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import SectionPaper from '../SectionPaper';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Typography from '@mui/material/Typography';
-import MapPlaceholder from '../MapPlaceholder';
 import type { MapTaskDetails } from '../../../../../client/types.gen';
 import LeafletMap from '../../LeafletMap';
 interface Props {
@@ -10,8 +9,11 @@ interface Props {
 }
 const FinalSection: React.FC<Props> = ({ mapTask }) => (
   <SectionPaper id="final" title="Final result" icon={<CheckCircleIcon fontSize="small" color="primary" />}>
-    <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
-      The final map applies the aggregated suitability map and excludes restricted areas. Different colours indicate suitability tiers (e.g., high/medium/low), or masked-out restricted areas.
+    <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
+      The final map removes constrained areas from the weighted-overlay suitability map, providing a clear visualization of locations suitable for solar farm development while respecting the defined constraints.
+    </Typography>
+    <Typography variant="body2" color="text.secondary" sx={{ mb: 0 }}>
+      Darker areas indicate higher overall suitability scores.
     </Typography>
     <LeafletMap fileUrl={mapTask.files?.find(file => file.file_type == 'final')?.file_path || ''} fileTag="final" mapHeight={450} />
   </SectionPaper>
