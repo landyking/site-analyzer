daa0479f5d061e99bc02c5ed7f25a5eabeaec2c6
landyking <landykingdom@gmail.com>
2025-10-02 19:57:55 +1300

    backend: remove unused imports from db.py

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,8 +1,6 @@
 from sqlmodel import Session, create_engine, select
 
-from app import crud
 from app.core.config import settings
-from app.models import UserDB, UserCreate, UserRole, UserStatus
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
