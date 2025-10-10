7029f174d1ed767be03a5f355dd2e0a2b2c4740a
landyking <landykingdom@gmail.com>
2025-10-10 15:13:32 +1300

    web: change alert severity from info to warning for demo mode

--- webfront/src/utils/ShowDemo.tsx
+++ webfront/src/utils/ShowDemo.tsx
@@ -4,7 +4,7 @@ import Alert from "@mui/material/Alert";
 export function ShowDemo() {
     if (import.meta.env.VITE_RELEASE_DEMO === '1') {
         return (
-            <Alert severity="info" sx={{ mb: 0 }}>
+            <Alert severity="warning" sx={{ mb: 0 }}>
                 Note: The system is in demo mode. You can explore the app using our demo account. 
                 Username: <strong>{import.meta.env.VITE_RELEASE_DEMO_USERNAME}</strong> and password: <strong>{import.meta.env.VITE_RELEASE_DEMO_PASSWORD}</strong>.
             </Alert>
